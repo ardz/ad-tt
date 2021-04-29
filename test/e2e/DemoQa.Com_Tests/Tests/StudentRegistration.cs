9aa596a79d12044251782676bf0a04a4c929caa4
dickyp <richard.peckett@outlook.com>
2021-04-29 17:24:02 +0100

    comments

--- test/e2e/DemoQa.Com_Tests/Tests/StudentRegistration.cs
+++ test/e2e/DemoQa.Com_Tests/Tests/StudentRegistration.cs
@@ -34,13 +34,21 @@ namespace DemoQa.Com_Tests.Tests
             string phoneNumber, string dob, string hobby, string address, string profile)
         {
             _tests.DemoQaPages.PageStudentRegistrationForm().NavigateTo();
+            
+            // deliberately writing gherkin here which isn't tied to a UI (anti-patten)
+            // the tests steps would then be hard coded to the UI which could change
+            // the types of behaviours could be tested in a subcutaneous (underneath the UI)
+            // manner too, beyond even an API to something like a system integration test which
+            // calls a handler from the application code for example
+            
+            // https://www.ministryoftesting.com/dojo/lessons/introduction-to-subcutaneous-testing
 
             // could be an example of a given here but there's not really any initial
             // system context you can specify here so it doesn't really work
             "Given a student has no existing account"
                 .x(() => { });
 
-            "When the student registers completing all mandatory fields"
+            "When the student registers providing all mandatory data"
                 .x(() =>
                 {
                     _tests.DemoQaPages.PageStudentRegistrationForm()
@@ -48,7 +56,7 @@ namespace DemoQa.Com_Tests.Tests
                         .SendEmail(email)
                         .SelectGender(gender)
                         .SendPhoneNumber(phoneNumber)
-                        // deliberate bug in the dob field, I strongly against automating
+                        // deliberate bug in the dob field, I'm strongly against automating
                         // complex controls like this
                         .SendDateOfBirth(dob)
                         .SelectHobby(hobby)
